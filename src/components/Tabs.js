66dd606ee575cdc7fd30018b7aa35986c5084d19
Areeb <areebkhan274427@gmail.com>
2023-05-18 14:48:26 +0530

    Initial Changes

---
+++ src/components/Tabs.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+
+
+function Tabs(props){
+    let [content,setContent] = useState('');
+    console.log(props);
+    // function contentSet(content){
+    //     setContent(content);
+    // }
+    const arrayDataItems = props.array.map((data) => <li onClick={()=>setContent(data.content)}>{data.title}</li>);
+
+    return(
+        <div>
+            <ul>{arrayDataItems}</ul>
+            {
+                content &&
+                <p>{content}</p>
+            }
+
+        </div>
+    )
+}
+
+export default Tabs;
